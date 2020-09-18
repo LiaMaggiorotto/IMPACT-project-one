a0bd5ad499ff2a25d93b9c68af5c72cf336995e5
Lia Sexton <liamaggiorotto@gmail.com>
2020-09-18 15:40:35 -0500

    adds completed model index

--- models/index.js
+++ models/index.js
@@ -5,5 +5,21 @@ const connectionString = "mongdb://localhost:27017/impact";
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    
-})
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then(function () {
+    console.log("Mongodb connected....");
+  })
+  .catch(function (error) {
+    console.log("Mongodb connection err", error);
+  });
+
+mongoose.connection.on("disconnect", function (event) {
+  console.log("mongodb disconnected", event);
+});
+
+module.exports = {
+    Product: require("./Product"),
+    User: require("./User")
+};
