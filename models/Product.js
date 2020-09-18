b09df4fd0a738caad8e8b9d68ec863346c506edd
Lia Sexton <liamaggiorotto@gmail.com>
2020-09-18 16:03:19 -0500

    adds product model

--- models/Product.js
+++ models/Product.js
@@ -4,10 +4,10 @@ const productSchema = new mongoose.Schema(
     {
     title: { type: String, required: true },
     price: { type: String, required: true },
-    imageLink: { type: String, required: false },
+    imageLink: { type: String },
     productLink: { type: String, required: true},
-    description: { type: String, required: false},
-    category: { type: String,  required: true},
+    description: { type: String },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: "User",  required: true},
   },
   {
     timestamps: true
