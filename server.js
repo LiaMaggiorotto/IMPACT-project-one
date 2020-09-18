b42fa74fb79a7461ae7ffc1157a2e916aad79aa4
Lia Sexton <liamaggiorotto@gmail.com>
2020-09-18 15:01:56 -0500

    add server configuration, server.js buckets

---
+++ server.js
@@ -0,0 +1,31 @@
+// --------------------- External Modules
+const express = require("express");
+const methodOverride = require("method-override");
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+// --------------------- Internal Modules
+
+
+
+// --------------------- Instanced Modules
+const app = express();
+
+
+// --------------------- Configuration
+const PORT = 4000;
+
+
+// --------------------- Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+
+// --------------------- Routes
+
+
+
+// --------------------- Server Listener
+app.listen(PORT, function () {
+    console.log(`Server is live and listening at http://localhost:${PORT}`);
+  });
