26c59a5639e1192244a719be9ea68b384136b581
Lia Sexton <liamaggiorotto@gmail.com>
2020-09-18 22:06:28 -0500

    redo: adds base for user controller

--- controllers/users.js
+++ controllers/users.js
@@ -5,5 +5,26 @@ const db = require("../models")
 
 // base route is /user
 
+//  ------------ index route
+
+
+//  ------------ new route 
+
+
+//  ------------ create route
+
+
+//  ------------ show route
+
+
+//  ------------ edit route
+
+
+//  ------------ update route
+
+
+//  ------------ delete route
+
+
 // Export
 module.exports = router;
